f29ef73d192cd57ffcef0d390b3ab545d63feebd
grischad <67879713+grischad@users.noreply.github.com>
2023-07-18 18:51:57 +0200

    feat: preview

--- pages/preview.js
+++ pages/preview.js
@@ -1,6 +1,49 @@
 import { WordPressTemplate } from '@faustwp/core';
+import { useAuth, getApolloAuthClient, useLogout } from "@faustwp/core";
+import { gql, useQuery } from "@apollo/client";
 
-export default function Preview(props) {
-  console.log('props', props)
+function AuthenticatedView(props) {
+  const client = getApolloAuthClient();
+  const { logout } = useLogout();
+  const { data, loading } = useQuery(
+    gql`
+      {
+        viewer {
+          posts {
+            nodes {
+              id
+              title
+            }
+          }
+          name
+        }
+      }
+    `,
+    { client }
+  );
+
+  if (loading) {
+    return <>Loading...</>;
+  }
+
   return <WordPressTemplate {...props} />;
+}
+
+export default function Page(props) {
+  const { isAuthenticated, isReady, loginUrl } = useAuth();
+
+  if (!isReady) {
+    return <>Loading...</>;
+  }
+
+  if (isAuthenticated === true) {
+    return <AuthenticatedView {...props} />;
+  }
+
+  return (
+    <>
+      <p>Welcome!</p>
+      <a href={loginUrl}>Login</a>
+    </>
+  );
 }
